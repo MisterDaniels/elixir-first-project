39f51c2e91c3599eeab9c978c757e3689c2feff4
Daniel Silva <dodosilva51@gmail.com>
2022-03-08 23:17:00 -0300

    Created first schema to get links
    Created first resolver to get links
    
    Editer http server to serve graphiql interface

---
+++ lib/community_web/schema.ex
@@ -0,0 +1,19 @@
+defmodule CommunityWeb.Schema do
+    use Absinthe.Schema
+
+    alias CommunityWeb.NewResolver
+
+    object :link do
+        field :id, non_null(:id)
+        field :url, non_null(:string)
+        field :description, non_null(:string)
+    end
+
+    query do
+        # This is the query entry point to our app
+        @desc "Get all links"
+        field :all_links, non_null(list_of(non_null(:link))) do
+            resolve(&NewsResolver.all_links/3)
+        end
+    end
+end
